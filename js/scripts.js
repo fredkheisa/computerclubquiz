2918007fd7a5f07ee257ae1aa10faa420e7e898c
Fred Khesia <you@example.com>
2018-09-22 21:10:30 +0300

    start editing js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var language = $("input:radio[name=answer]:checked").val(20%);
+var react = $("input:radio[name=answer]:checked").val(20%);
+var return = $("input:radio[name=answer]:checked").val(20%);
+var switch = $("input:radio[name=answer]:checked").val(20%);
+var 6 = $("input:radio[name=answer]:checked").val(20%);
