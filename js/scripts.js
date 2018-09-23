b7e9c3d77b39eec6503343b2fc7edff08c78aff9
Fred Khesia <you@example.com>
2018-09-24 00:18:24 +0300

    added the classes afresh

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 function check(){
     var question1 = document.Quiz.question1.value;
     var question2 = document.Quiz.question2.value;
+    var question3 = document.Quiz.question2.value;
+    var question4 = document.Quiz.question2.value;
+    var question5 = document.Quiz.question2.value;
     var right =0;
 
 
@@ -10,6 +13,16 @@ function check(){
     if (question2="correct"){
         right= right+20;
     };
+    if (question3="correct"){
+        right= right+20;
+    };
+    if (question4="correct"){
+        right= right+20;
+    };
+    if (question5="correct"){
+        right= right+20;
+    };
+    
  
     document.getElementById("fifth-p").style.visibility="visible";
     document.getElementById("correctNumber").innerHTML="you've scored" + right+ "%";
