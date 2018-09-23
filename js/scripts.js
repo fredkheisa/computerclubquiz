5640a9e65f4410c6e76786a20a2913c17879dbb7
Fred Khesia <you@example.com>
2018-09-23 18:49:39 +0300

    js and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-var language = $("input:radio[name=answer]:checked").val(20%);
-var react = $("input:radio[name=answer]:checked").val(20%);
-var return = $("input:radio[name=answer]:checked").val(20%);
-var switch = $("input:radio[name=answer]:checked").val(20%);
-var 6 = $("input:radio[name=answer]:checked").val(20%);
+
+
+function check () {
+
+
+
+
+
