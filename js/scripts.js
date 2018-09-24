649f50a7c4a64e8275c61a360689b782973de521
Fred Khesia <you@example.com>
2018-09-24 10:46:13 +0300

    js runs correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
 function check(){
     var question1 = document.Quiz.question1.value;
     var question2 = document.Quiz.question2.value;
-    var question3 = document.Quiz.question2.value;
-    var question4 = document.Quiz.question2.value;
-    var question5 = document.Quiz.question2.value;
+    var question3 = document.Quiz.question3.value;
+    var question4 = document.Quiz.question4.value;
+    var question5 = document.Quiz.question5.value;
     var right =0;
-
-
-    if(question1="correct"){
-        right=right  +20; 
-    };
-    if (question2="correct"){
+ 
+ 
+    if(question1 == "correct"){
         right= right+20;
     };
-    if (question3="correct"){
+    if (question2 == "correct"){
         right= right+20;
     };
-    if (question4="correct"){
+    if (question3== "correct"){
         right= right+20;
     };
-    if (question5="correct"){
+    if (question4=="correct"){
         right= right+20;
     };
-    
+    if (question5=="correct"){
+        right= right+20;
+    };
+ 
  
     document.getElementById("fifth-p").style.visibility="visible";
-    document.getElementById("correctNumber").innerHTML="you've scored" + right+ "%";
-};
+    document.getElementById("correctNumber").innerHTML="here is your score" + right+ "%";
+ };
 
 $(document).ready(function() {
     $("Submit").click(function() {
