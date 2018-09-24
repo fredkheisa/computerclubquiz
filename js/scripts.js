e80e8905052689bd305bcb023ee763f9edecfb92
Fred Khesia <you@example.com>
2018-09-24 08:45:05 +0300

    README format edited

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function check(){
     var question1 = document.Quiz.question1.value;
     var question2 = document.Quiz.question2.value;
-    var question3 = document.Quiz.question3.value;
-    var question4 = document.Quiz.question4.value;
-    var question5 = document.Quiz.question5.value;
+    var question3 = document.Quiz.question2.value;
+    var question4 = document.Quiz.question2.value;
+    var question5 = document.Quiz.question2.value;
     var right =0;
 
 
